5d3e46f8d74ef52d23ef2e225b9abf7c0a04688e
Hiren Variava <hvariava@gmail.com>
2021-12-13 18:25:35 -0800

    moved introductory comments to README file

--- index.js
+++ index.js
@@ -3,23 +3,7 @@
 // Author: Hiren Variava
 // Date: Dec 13, 2021
 //
-// As of creation of this script, Twilio didn't provide a way to bulk call their Lookup API.
-// This script was created to use Lookup API from Twilio in a bulk manner.
-//
-// The script was created as a quick solution to a problem that I was facing, and thus
-// it's not following any sort of best practices.
-//
-// The purpose of this script was just to get phone type from Lookup API,
-// but you can modify the script to get more information out of this API if need be.
-//
-// Input file format: one phone number per line
-// Output file format: one phone number and phone type per line separted by comma
-//
-// Because of the concurrent nature of this script, the order of the 
-// phone numbers in output file may be different from input file.
-//
-// For Twilio credentials, please refer to: https://www.twilio.com/docs/iam/credentials/api
-// Twilio API docs: https://www.twilio.com/docs
+// Please refer to README file for information about this project.
 
 // credentials
 // const accountSid = process.env.TWILIO_ACCOUNT_SID;
